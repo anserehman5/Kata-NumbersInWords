b8151e2e1bc6bd3dd3154264827cf4b339919eed
Anse <anserehman5@gmail.com>
2021-07-30 17:25:05 +0100

    Initial commit

---
+++ Kata-NumbersInWords/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Kata_NumbersInWords
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberToTextButton = new System.Windows.Forms.Button();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.textTextBox = new System.Windows.Forms.TextBox();
+            this.warningLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.wordsToNumberButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // numberToTextButton
+            // 
+            this.numberToTextButton.Location = new System.Drawing.Point(165, 301);
+            this.numberToTextButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numberToTextButton.Name = "numberToTextButton";
+            this.numberToTextButton.Size = new System.Drawing.Size(231, 68);
+            this.numberToTextButton.TabIndex = 0;
+            this.numberToTextButton.Text = "Convert Amount to Text";
+            this.numberToTextButton.UseVisualStyleBackColor = true;
+            this.numberToTextButton.Click += new System.EventHandler(this.numberToTextButton_Click);
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(165, 131);
+            this.numberTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numberTextBox.Multiline = true;
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(252, 60);
+            this.numberTextBox.TabIndex = 1;
+            // 
+            // textTextBox
+            // 
+            this.textTextBox.Location = new System.Drawing.Point(165, 469);
+            this.textTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textTextBox.Multiline = true;
+            this.textTextBox.Name = "textTextBox";
+            this.textTextBox.Size = new System.Drawing.Size(252, 60);
+            this.textTextBox.TabIndex = 2;
+            // 
+            // warningLabel
+            // 
+            this.warningLabel.AutoSize = true;
+            this.warningLabel.Location = new System.Drawing.Point(214, 40);
+            this.warningLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.warningLabel.Name = "warningLabel";
+            this.warningLabel.Size = new System.Drawing.Size(144, 20);
+            this.warningLabel.TabIndex = 3;
+            this.warningLabel.Text = "Numbers to Text";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 134);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Amount:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 469);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Amount As Text:";
+            // 
+            // wordsToNumberButton
+            // 
+            this.wordsToNumberButton.Location = new System.Drawing.Point(757, 301);
+            this.wordsToNumberButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.wordsToNumberButton.Name = "wordsToNumberButton";
+            this.wordsToNumberButton.Size = new System.Drawing.Size(231, 68);
+            this.wordsToNumberButton.TabIndex = 6;
+            this.wordsToNumberButton.Text = "Convert Amount to Numbers";
+            this.wordsToNumberButton.UseVisualStyleBackColor = true;
+            this.wordsToNumberButton.Click += new System.EventHandler(this.wordsToNumberButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(757, 137);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 60);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(556, 466);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(181, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Amount As Numbers:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 215);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "e.g. 205.34";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(757, 466);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 60);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(556, 137);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Enter Amount:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(556, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(423, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "e.g. One Thousand Pounds and Thirty-Five Pennies";
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Location = new System.Drawing.Point(528, -1);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 627);
+            this.label7.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(753, 40);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(144, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Text to Numbers";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 629);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.wordsToNumberButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.warningLabel);
+            this.Controls.Add(this.textTextBox);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.numberToTextButton);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button numberToTextButton;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.TextBox textTextBox;
+        private System.Windows.Forms.Label warningLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button wordsToNumberButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Kata-NumbersInWords/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Globalization;
+
+
+namespace Kata_NumbersInWords
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            Numbers.unitsMap[0] = "Zero";
+            Numbers.unitsMap[1] = "One";
+            Numbers.unitsMap[2] = "Two";
+            Numbers.unitsMap[3] = "Three";
+            Numbers.unitsMap[4] = "Four";
+            Numbers.unitsMap[5] = "Five";
+            Numbers.unitsMap[6] = "Six";
+            Numbers.unitsMap[7] = "Seven";
+            Numbers.unitsMap[8] = "Eight";
+            Numbers.unitsMap[9] = "Nine";
+            Numbers.unitsMap[10] = "Ten";
+            Numbers.unitsMap[11] = "Eleven";
+            Numbers.unitsMap[12] = "Twelve";
+            Numbers.unitsMap[13] = "Thirteen";
+            Numbers.unitsMap[14] = "Fourteen";
+            Numbers.unitsMap[15] = "Fifteen";
+            Numbers.unitsMap[16] = "Sixteen";
+            Numbers.unitsMap[17] = "Seventeen";
+            Numbers.unitsMap[18] = "Eighteen";
+            Numbers.unitsMap[19] = "Nineteen";
+
+
+            Numbers.tensMap[0] = "Zero";
+            Numbers.tensMap[1] = "Ten";
+            Numbers.tensMap[2] = "Twenty";
+            Numbers.tensMap[3] = "Thirty";
+            Numbers.tensMap[4] = "Forty";
+            Numbers.tensMap[5] = "Fifty";
+            Numbers.tensMap[6] = "Sixty";
+            Numbers.tensMap[7] = "Seventy";
+            Numbers.tensMap[8] = "Eighty";
+            Numbers.tensMap[9] = "Ninety";
+
+
+            Numbers.numberNames.Add("Zero", 0);
+            Numbers.numberNames.Add("One", 1);
+            Numbers.numberNames.Add("Two", 2);
+            Numbers.numberNames.Add("Three", 3);
+            Numbers.numberNames.Add("Four", 4);
+            Numbers.numberNames.Add("Five", 5);
+            Numbers.numberNames.Add("Six", 6);
+            Numbers.numberNames.Add("Seven", 7);
+            Numbers.numberNames.Add("Eight", 8);
+            Numbers.numberNames.Add("Nine", 9);
+            Numbers.numberNames.Add("Ten", 10);
+            Numbers.numberNames.Add("Eleven", 11);
+            Numbers.numberNames.Add("Twelve", 12);
+            Numbers.numberNames.Add("Thirteen", 13);
+            Numbers.numberNames.Add("Fourteen", 14);
+            Numbers.numberNames.Add("Fifteen", 15);
+            Numbers.numberNames.Add("Sixteen", 16);
+            Numbers.numberNames.Add("Seventeen", 17);
+            Numbers.numberNames.Add("Eighteen", 18);
+            Numbers.numberNames.Add("Nineteen", 19);
+
+
+            Numbers.numberNames.Add("Twenty", 20);
+            Numbers.numberNames.Add("Thirty", 30);
+            Numbers.numberNames.Add("Forty", 40);
+            Numbers.numberNames.Add("Fifty", 50);
+            Numbers.numberNames.Add("Sixty", 60);
+            Numbers.numberNames.Add("Seventy", 70);
+            Numbers.numberNames.Add("Eighty", 80);
+            Numbers.numberNames.Add("Ninety", 90);
+
+
+            Numbers.numberNames.Add("Hundred", 100);
+            Numbers.numberNames.Add("Thousand", 1000);
+            Numbers.numberNames.Add("Million", 1000000);
+
+
+            InitializeComponent();
+
+
+        }
+
+        static class Numbers
+        {
+            //Hashtable to store numbers with corresponding names
+            public static Hashtable numberNames = new Hashtable();
+
+            //Array of strings to hold all possible units as words
+            public static string[] unitsMap = new string[20];
+
+            //Array of strings to hold all possible tens as words
+            public static string[] tensMap = new string[10];
+        }
+
+        public static string NumberToWords(int number)
+        {
+            if (number == 0)
+                return "Zero Pounds";
+
+            //If number is negative recall function, but add "minus" to front of the result
+            if (number < 0)
+                return "Minus " + NumberToWords(Math.Abs(number));
+
+            //Decalre string which will be returned as the result
+            string words = "";
+
+            //If number divided by 1 Million is larger than 1, appened recalled fucntion + "Million" to the final string
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " Million ";
+                number %= 1000000;
+            }
+
+            //If number divided by 1 Thousand is larger than 1, appened recalled fucntion + "Thousand" to the final string
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " Thousand ";
+                number %= 1000;
+            }
+
+            //If number divided by 1 Hundred is larger than 1, appened recalled fucntion + "Hundred" to the final string
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " Hundred ";
+                number %= 100;
+            }
+
+            //If number is larger than 1
+            if (number > 0)
+            {
+                //If the result string is not empty, meaning the number has gone though at least one of the previous "if statements" - append "and" the result string
+                if (words != "")
+                    words += "and ";
+
+                //If number is 19 or smaller, append the correspending unit as a word to the final string
+                // e.g. unitsMap[0] = "Zero", unitsMap[6] = "Six".
+                if (number < 20)
+                    words += Numbers.unitsMap[number];
+                else
+                {
+                    //If number is 20 or bigger, append the correspending tens as a word to the final string
+                    // e.g. tensMap[20 / 10] = "Twenty", unitsMap[45 / 10] = "Forty".
+                    words += Numbers.tensMap[number / 10];
+
+                    //If number % 10 is bigger than 0 (meaning the number does not end in 0 e.g. 45)
+                    //append "-" + the corresponding unit as a word to the final string e.g. 45 = "-five"
+                    if ((number % 10) > 0)
+                        words += "-" + Numbers.unitsMap[number % 10];
+                }
+                
+            }
+
+            return words;
+        }
+
+        public string DecimalToWords(decimal number)
+        {
+            //Works the same/with NumberToWords but checks if there is any numbers after the decimal
+            //If there is it first calls NumberToWords for the decimal number (Pennies)
+            //And then does the regular call for the whole number (Pounds)
+
+            if (number == 0)
+                return "Zero Pounds";
+
+            if (number < 0)
+                return "Minus " + DecimalToWords(Math.Abs(number));
+
+            string words = "";
+
+            int intPortion = (int)number;
+            decimal fraction = (number - intPortion) * 100;
+            int decPortion = (int)fraction;
+
+            words = NumberToWords(intPortion);
+            words += " Pounds";
+            //If the amount of Pennies is bigger than 0, appened the number and Pennies to the final string
+            if (decPortion > 0)
+            {
+                words += " and ";
+                words += NumberToWords(decPortion);
+                words += " Pennies";
+
+            }
+            return words;
+        }
+
+        public string WordsToNumber(string number)
+        {
+
+            // Creates a TextInfo based on the "en-US" culture.
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
+            //Converts text to title case so words can be detected later
+            string textToBeConverted = myTI.ToTitleCase(textBox1.Text);
+
+
+            //Split the text into two string - everything before Pounds and everything after (Pounds and Pennies)
+            //If there is nothing after Pounds, remove the empty element
+            string[] strArrayPoundsPennies = textToBeConverted.Split(new string[] { "Pounds" }, StringSplitOptions.RemoveEmptyEntries);
+
+            string poundsVal = strArrayPoundsPennies[0];
+            string penniesVal = "0";
+            string[] strArrayPounds;
+            string[] strArrayPennies;
+            int value = 0;
+            int tempValue = 0;
+
+            //If there was 2 elements in the string array (If pennies were included in the amount)
+            if (strArrayPoundsPennies.Length > 1)
+            {
+                //Remove extra words from the pennies string so you are just left with a string of the amount of pennies
+                penniesVal = strArrayPoundsPennies[1];
+                penniesVal = penniesVal.Replace(" Pennies", "");
+                penniesVal = penniesVal.Replace(" And", "");
+
+                //Create array of the words in the pennies array by spliiting spaces and '-'
+                //e.g. Forty-Five = "Forty" and "Five"
+                strArrayPennies = penniesVal.Split(' ', '-');
+
+
+                //Loop through strings in array of penny values
+                foreach (string s in strArrayPennies)
+                {
+                    //if string equal to any of these - take the string and find the corresponding value by using the hashtable and add it to value
+                    if (Equals(s, "Hundred") || Equals(s, "Thousand") || Equals(s, "Million"))
+                    {
+                        value += tempValue * int.Parse(Numbers.numberNames[s].ToString());
+                        tempValue = 0;
+                    }
+                    else
+                    {
+                        //If the value can be found in the hashtable, add the corresponding value to tempValue
+                        if (Numbers.numberNames.ContainsKey(s))
+                        {
+                            tempValue += int.Parse(Numbers.numberNames[s].ToString());
+                        }
+                    }
+                }
+
+                //Add value and tempValue and convert it to a string for the final amount of pennies.
+                penniesVal = (value + tempValue).ToString();
+            }
+
+            //Do the same as above, but for Pounds
+            poundsVal = poundsVal.Replace(" Pounds", "");
+            poundsVal = poundsVal.Replace(" And", "");
+
+
+            strArrayPounds = poundsVal.Split(' ', '-');
+
+            value = 0;
+            tempValue = 0;
+
+
+            foreach (string s in strArrayPounds)
+            {
+                if (Equals(s, "Hundred") || Equals(s, "Thousand") || Equals(s, "Million"))
+                {
+                    value += tempValue * int.Parse(Numbers.numberNames[s].ToString());
+                    tempValue = 0;
+                }
+                else
+                {
+                    if (Numbers.numberNames.ContainsKey(s))
+                    {
+                        tempValue += int.Parse(Numbers.numberNames[s].ToString());
+                    }
+                }
+            }
+
+            poundsVal = (value + tempValue).ToString();
+
+            //Append the Pounds and Pennies together with a decimal between them
+            return poundsVal + "." + penniesVal;
+
+        }
+
+        private void numberToTextButton_Click(object sender, EventArgs e)
+        {
+
+            try {
+                //Convert string amount to decimal and call function to convert
+                decimal initialString;
+                initialString = decimal.Parse(numberTextBox.Text);
+
+                textTextBox.Text = DecimalToWords(initialString);
+            }
+            catch
+            {
+                //Error handling
+                    MessageBox.Show("Please enter a valid amount. e.g. 568.23");
+            }
+  
+
+        }
+
+
+        private void wordsToNumberButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Call function to convert and set textBox2's text to be the result
+                textBox2.Text = WordsToNumber(textBox1.Text);
+            }
+            catch
+            {
+                //Error handling
+                MessageBox.Show("Please enter a valid amount. e.g. One Thousand and Twenty Pounds and Thirty-Five Pennies");
+            }
+            
+        }
+
+    }
+}
